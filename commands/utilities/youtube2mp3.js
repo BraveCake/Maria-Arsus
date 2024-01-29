ab172c504c51c92f7a00385a4ad315a649f4666d
BraveCake <aliotaku241@gmail.com>
2024-01-29 02:01:30 +0200

    fix converted file names not being fully sanitized

--- commands/utilities/youtube2mp3.js
+++ commands/utilities/youtube2mp3.js
@@ -14,14 +14,17 @@ module.exports = {
   async execute(interaction) {
     await interaction.deferReply();
     const videoUrl = interaction.options.get('url').value;
-    const videoID = ytdl.getURLVideoID(videoUrl);
-    const videoTitle = (await ytdl.getInfo(videoID)).videoDetails.title;
-    const filePath = `audio/${videoTitle}.mp3`;
-    const stream = ytdl(videoUrl, { filter: 'audioonly' }).pipe(fs.createWriteStream(filePath));
-    await new Promise(resolve => stream.on("finish", resolve));
-    await interaction.editReply({ content: 'Here is the file you requested:', files: [filePath] });
-    fs.unlinkSync(filePath);
-
-
+	  try{
+      const videoID = ytdl.getURLVideoID(videoUrl);
+      const videoTitle = (await ytdl.getInfo(videoID)).videoDetails.title.replace(/[^a-z0-9]/gi, '_').toLowerCase();
+      const filePath = `audio/${videoTitle}.mp3`;
+      const stream = ytdl(videoUrl, { filter: 'audioonly' }).pipe(fs.createWriteStream(filePath));
+      await new Promise(resolve => stream.on("finish", resolve));
+      await interaction.editReply({ content: 'Here is the file you\'ve requested:', files: [filePath] });
+      fs.unlinkSync(filePath);
+    }
+    catch({name,msg}){
+      console.log(`an error ${name} has occurred ${msg}`);
+     }
   }
 };
